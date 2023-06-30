2c14d49fc69cca604c62c12f25b11d53d55f86b0
ijaz35 <ijaz.hossain1@gmail.com>
2023-07-01 00:34:00 +0600

    contact section added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,9 +7,11 @@ const About = () => {
         <div className={styles.container}>
             <div className={styles.imgContainer}>
                 <Image src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" fill="true" alt="about banner" className={styles.image} />
-                <div>
-                    <h1></h1>
-                    <h2></h2>
+                <div className={styles.imgText}>
+                    <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+                    <h2 className={styles.imgDesc}>
+                        Handcrafting award winning digital experiences
+                    </h2>
                 </div>
             </div>
             <div className={styles.textContainer}>
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import styles from './page.module.css'
+import Image from 'next/image';
+import Button from '../components/Button/Button';
 const Contact = () => {
     return (
         <div className={styles.container}>
-            Contact
+            <h1 className={styles.title}>Let&lsquo;s Keep in Touch</h1>
+            <div className={styles.content}>
+                <div className={styles.imgContainer}>
+                    <Image src="/contact.png" fill={true} alt="contact image" className={styles.image} />
+                </div>
+                <form className={styles.form}>
+                    <input type="text" placeholder='Enter your name' className={styles.input} />
+                    <input type="email" placeholder='Enter your email' className={styles.input} />
+                    <textarea name="description" id="" cols="30" rows="10" className={styles.textarea} placeholder='Enter your message'></textarea>
+                    <Button url="#" text="Send" />
+                </form>
+            </div>
         </div>
     );
 };
