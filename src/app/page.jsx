cc07034b10f493f74bd6b014e8c32286f2961fa6
ijaz35 <ijaz.hossain1@gmail.com>
2023-06-30 08:50:27 +0600

    home section added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,22 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import bannerImg from 'public/hero.png'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.description}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See our works</button>
+      </div>
+      <div className={styles.item}>
+
+        <Image src={bannerImg} alt="banner image" className={styles.images} />
+      </div>
     </div>
   )
 }
