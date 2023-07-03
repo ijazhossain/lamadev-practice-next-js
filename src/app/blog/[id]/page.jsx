df047caa05a2faaf271ad1f43d19481f06926929
ijaz35 <ijaz.hossain1@gmail.com>
2023-07-03 08:30:54 +0600

    data fetching in server side

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import styles from './page.module.css'
 import Image from 'next/image';
-const BlogPost = () => {
+import { notFound } from 'next/navigation';
+async function getData(id) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        cache: 'no-store',
+    })
+    if (!res.ok) {
+        return notFound()
+    }
+    return res.json()
+}
+const BlogPost = async ({ params }) => {
+    const data = await getData(params.id)
+    console.log(data);
     return (
         <div className={styles.container}>
             <div className={styles.upperContainer}>
                 <div className={styles.content}>
-                    <h1 className={styles.title}>Lorem ipsum, dolor sit amet consectetur adipisicing.</h1>
+                    <h1 className={styles.title}>{data.title}</h1>
                     <p className={styles.desc}>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora consequatur error possimus officia provident rem consectetur saepe quidem inventore deleniti neque sequi architecto nihil laboriosam vitae, repellat nam eaque. Iste.
                     </p>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,45 +2,35 @@ import React from 'react';
 import styles from './page.module.css'
 import Image from 'next/image';
 import Link from 'next/link';
-const BLog = () => {
+import { notFound } from 'next/navigation'
+
+async function getData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: "no-store", })
+    if (!res.ok) {
+        return notFound()
+    }
+    return res.json()
+}
+const BLog = async () => {
+    const data = await getData()
+    console.log(data);
     return (
         <div className={styles.mainContainer}>
-            <Link href="/blog/testId">
-                <div className={styles.container}>
-                    <div className={styles.imageContainer}>
-                        <Image width={400}
-                            height={250} src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className={styles.image} />
+            {
+                data.map(item => <Link href="/blog/testId" key={data.id}>
+                    <div className={styles.container}>
+                        <div className={styles.imageContainer}>
+                            <Image width={400}
+                                height={250} src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className={styles.image} />
+                        </div>
+                        <div className={styles.content}>
+                            <h1 className={styles.title}>{item.title}</h1>
+                            <p className={styles.desc}>{item.body}</p>
+                        </div>
                     </div>
-                    <div className={styles.content}>
-                        <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
-                        <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea cumque dolor veritatis repellat ab, inventore repellendus cupiditate ipsa sint, molestias fugiat repudiandae neque quam quo eaque illo praesentium porro tenetur similique. Similique quos corporis inventore, atque neque sequi deleniti consectetur ut voluptas et non suscipit aliquid quod odio illo.</p>
-                    </div>
-                </div>
-            </Link>
-            <Link href="/blog/testId">
-                <div className={styles.container}>
-                    <div className={styles.imageContainer}>
-                        <Image width={400}
-                            height={250} src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className={styles.image} />
-                    </div>
-                    <div className={styles.content}>
-                        <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
-                        <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea cumque dolor veritatis repellat ab, inventore repellendus cupiditate ipsa sint, molestias fugiat repudiandae neque quam quo eaque illo praesentium porro tenetur similique. Similique quos corporis inventore, atque neque sequi deleniti consectetur ut voluptas et non suscipit aliquid quod odio illo.</p>
-                    </div>
-                </div>
-            </Link>
-            <Link href="/blog/testId">
-                <div className={styles.container}>
-                    <div className={styles.imageContainer}>
-                        <Image width={400}
-                            height={250} src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className={styles.image} />
-                    </div>
-                    <div className={styles.content}>
-                        <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
-                        <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea cumque dolor veritatis repellat ab, inventore repellendus cupiditate ipsa sint, molestias fugiat repudiandae neque quam quo eaque illo praesentium porro tenetur similique. Similique quos corporis inventore, atque neque sequi deleniti consectetur ut voluptas et non suscipit aliquid quod odio illo.</p>
-                    </div>
-                </div>
-            </Link>
+                </Link>)
+            }
+
         </div>
     );
 };
