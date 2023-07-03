40d857457e3a3b3a7b3d6ba175a3176263638b36
ijaz35 <ijaz.hossain1@gmail.com>
2023-07-03 12:12:13 +0600

    locally adding data

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,43 +2,35 @@ import React from 'react';
 import styles from './page.module.css'
 import Button from '@/app/components/Button/Button';
 import Image from 'next/image';
+import { items } from './data'
+import { notFound } from 'next/navigation';
+const getData = (cat) => {
+    const data = items[cat];
+    if (data) {
+        return data;
+    }
+    return notFound();
+}
 const Category = ({ params }) => {
+    const data = getData(params.category)
     return (
         <div className={styles.container}>
             <h1>{params.category}</h1>
-            <div className={styles.item}>
-                <div className={styles.content}>
-                    <h1 className={styles.catTitle}>Minimal Single Product</h1>
-                    <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?</p>
-                    <Button url="#" text="See More" />
-                </div>
-                <div className={styles.imgContainer}>
-                    <Image src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg" alt="category image" fill="true" className='image' />
+            {
+                data.map(item => <div key={item.id} className={styles.item}>
+                    <div className={styles.content}>
+                        <h1 className={styles.catTitle}>{item.title}</h1>
+                        <p className={styles.desc}>{item.desc}</p>
+                        <Button url="#" text="See More" />
+                    </div>
+                    <div className={styles.imgContainer}>
+                        <Image src={item.image} alt="category image" fill="true" className='image' />
 
-                </div>
-            </div>
-            <div className={styles.item}>
-                <div className={styles.content}>
-                    <h1 className={styles.catTitle}>Minimal Single Product</h1>
-                    <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?</p>
-                    <Button url="#" text="See More" />
-                </div>
-                <div className={styles.imgContainer}>
-                    <Image src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg" alt="category image" fill="true" className='image' />
+                    </div>
+                </div>)
+            }
 
-                </div>
-            </div>
-            <div className={styles.item}>
-                <div className={styles.content}>
-                    <h1 className={styles.catTitle}>Minimal Single Product</h1>
-                    <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?</p>
-                    <Button url="#" text="See More" />
-                </div>
-                <div className={styles.imgContainer}>
-                    <Image src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg" alt="category image" fill="true" className='image' />
 
-                </div>
-            </div>
         </div>
     );
 };
