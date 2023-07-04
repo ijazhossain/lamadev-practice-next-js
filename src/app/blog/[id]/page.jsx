3edb89f72b068b235a4e039e4c71561188cea86c
ijaz35 <ijaz.hossain1@gmail.com>
2023-07-04 10:57:51 +0600

    api added

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 async function getData(id) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
         cache: 'no-store',
     })
     if (!res.ok) {
@@ -20,19 +20,27 @@ const BlogPost = async ({ params }) => {
                 <div className={styles.content}>
                     <h1 className={styles.title}>{data.title}</h1>
                     <p className={styles.desc}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora consequatur error possimus officia provident rem consectetur saepe quidem inventore deleniti neque sequi architecto nihil laboriosam vitae, repellat nam eaque. Iste.
+                        {data.desc}
                     </p>
+                    <div className={styles.author}>
+                        <Image
+                            src={data.img}
+                            alt=""
+                            width={40}
+                            height={40}
+                            className={styles.avatar}
+                        />
+                        <span className={styles.username}>{data.username}</span>
+                    </div>
 
                 </div>
                 <div className={styles.imgContainer}>
                     <Image
-                        fill="true" src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className={styles.image} alt="" />
+                        fill="true" src={data.img} className={styles.image} alt="" />
                 </div>
             </div>
             <div className={styles.lowerContainer}>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia facere doloremque minus omnis dolorem porro unde autem fuga vel impedit a dolorum pariatur provident, natus dignissimos amet consectetur fugit dolor sequi id eius temporibus. Natus amet ut optio! Magnam reprehenderit facere maxime optio sunt, cum earum atque repellat reiciendis sequi?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo accusantium hic nam ullam cum, dolor repellendus officia iste perspiciatis voluptatem?</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum dolores placeat sequi repellat, eum aperiam facere autem harum ipsam voluptate iure enim suscipit est accusantium aliquam! Vel delectus saepe amet nihil, consectetur eius blanditiis doloremque ut. Doloremque, tempore hic eligendi veniam molestias quam voluptatem culpa totam odit suscipit officiis mollitia esse non quidem facere itaque ratione praesentium accusamus? Sit doloribus dolor, aut saepe harum quae veniam excepturi minus alias. Tempore recusandae alias, illo temporibus eos quia voluptatem numquam officia eum itaque provident veritatis dolorum ex laudantium! Hic, esse quo, tempora ipsum autem labore sint numquam consequuntur atque incidunt eligendi. Tempore!</p>
+                <p>{data.content}</p>
             </div>
         </div>
     );
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { notFound } from 'next/navigation'
 
 async function getData() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: "no-store", })
+    const res = await fetch('http://localhost:3000/api/posts', { cache: "no-store", })
     if (!res.ok) {
         return notFound()
     }
@@ -17,15 +17,15 @@ const BLog = async () => {
     return (
         <div className={styles.mainContainer}>
             {
-                data.map(item => <Link href="/blog/testId" key={data.id}>
+                data.map(item => <Link href={`/blog/${item._id}`} key={item._id}>
                     <div className={styles.container}>
                         <div className={styles.imageContainer}>
                             <Image width={400}
-                                height={250} src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className={styles.image} />
+                                height={250} src={item.img} alt="" className={styles.image} />
                         </div>
                         <div className={styles.content}>
                             <h1 className={styles.title}>{item.title}</h1>
-                            <p className={styles.desc}>{item.body}</p>
+                            <p className={styles.desc}>{item.desc}</p>
                         </div>
                     </div>
                 </Link>)
