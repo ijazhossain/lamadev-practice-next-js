3e85e6ff4081d790d89f01084449a0e7e6c32977
ijaz35 <ijaz.hossain1@gmail.com>
2023-07-18 17:27:39 +0600

    user added to DB

---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,26 @@
+import connect from "@/app/utils/db";
+import User from "@/models/User";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs"
+
+export const POST = async (request) => {
+    const { name, email, password } = await request.json()
+    await connect();
+    const hashPassword = await bcrypt.hash(password, 5)
+    const newUser = new User({
+        name,
+        email,
+        password: hashPassword,
+    })
+    try {
+        await newUser.save()
+        return new NextResponse("Use has created", {
+            status: 201,
+        });
+
+    } catch (err) {
+        return new NextResponse(err.message, {
+            status: 500,
+        });
+    }
+}
